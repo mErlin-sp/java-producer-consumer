bf7650f0a48c3d641128b69d823b634763380543
Oleksandr <merlin.ua.1@gmail.com>
2023-04-30 02:40:33 +0300

    Code cleanup

--- src/Drop.java
+++ src/Drop.java
@@ -1,7 +1,8 @@
 public class Drop {
-    // Message sent from producer
-    // to consumer.
+
+    // Message sent from producer to consumer.
     private String message;
+
     // True if consumer should wait
     // for producer to send message,
     // false if producer should wait for
@@ -9,37 +10,40 @@ public class Drop {
     private boolean empty = true;
 
     public synchronized String take() {
-        // Wait until message is
-        // available.
+
+        // Wait until message is available.
         while (empty) {
             try {
                 wait();
             } catch (InterruptedException ignored) {
             }
         }
+
         // Toggle status.
         empty = true;
-        // Notify producer that
-        // status has changed.
+
+        // Notify producer that status has changed.
         notifyAll();
         return message;
     }
 
     public synchronized void put(String message) {
-        // Wait until message has
-        // been retrieved.
+
+        // Wait until message has been retrieved.
         while (!empty) {
             try {
                 wait();
             } catch (InterruptedException ignored) {
             }
         }
+
         // Toggle status.
         empty = false;
+
         // Store message.
         this.message = message;
-        // Notify consumer that status
-        // has changed.
+
+        // Notify consumer that status has changed.
         notifyAll();
     }
 }
